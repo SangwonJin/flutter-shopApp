88daf0112ffeea13540bb2e0a050ee0bcee28227
SangwonJin <clubwonni@gmail.com>
2020-11-19 16:15:47 +0900

    added cart function that calculate total price of item in

---
+++ lib/providers/cart.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem(
+      {@required this.id,
+      @required this.title,
+      @required this.quantity,
+      @required this.price});
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _itmes = {};
+
+  Map<String, CartItem> get itmes {
+    return {..._itmes};
+  }
+
+  int get itemCount {
+    return _itmes == null ? 0 : _itmes.length;
+  }
+
+  double get totalAmount {
+    double total = 0;
+    _itmes.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_itmes.containsKey(productId)) {
+      //change quantity..
+      _itmes.update(
+          productId,
+          (existingCartItem) => CartItem(
+              id: existingCartItem.id,
+              title: existingCartItem.title,
+              price: existingCartItem.price,
+              quantity: existingCartItem.quantity + 1));
+    } else {
+      _itmes.putIfAbsent(
+          productId,
+          () => CartItem(
+              id: DateTime.now().toString(),
+              title: title,
+              price: price,
+              quantity: 1));
+    }
+    notifyListeners();
+  }
+}
